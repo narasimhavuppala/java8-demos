07213570a08ce01ac6964df0fe28ebcce39a1f5c
narasimhavuppala <nraovuppala@Wgmail.com>
2018-09-29 22:41:07 -0500

    Sequential and Parallel Streams: dm

---
+++ Streams/src/SequentialParallelStreams.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.stream.Stream;
+
+public class SequentialParallelStreams {
+
+	public static void main(String[] args) {
+
+		Collection<Integer> numbers = new ArrayList<>();
+		for (int i = 0; i < 15; i++) {
+			numbers.add(i);
+		}
+		Stream<Integer> numberStreaam = numbers.stream();
+		System.out.println(numberStreaam.isParallel());
+		Stream<Integer> parallelStream = numbers.parallelStream();
+		System.out.println(parallelStream.isParallel());
+
+		parallelStream.sequential().parallel().sequential();
+
+		System.out.println(parallelStream.isParallel());
+
+		parallelStream.sequential().parallel().sequential().parallel();
+
+		System.out.println(parallelStream.isParallel());
+		
+		//Takeawy-1 : parallel is a flag....last write wins.
+		//Takeaway-2: A Stream is Parallel or sequwntial but can not be both
+	}
+
+}
