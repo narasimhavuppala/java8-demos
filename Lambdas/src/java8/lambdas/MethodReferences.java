7f53b50c462ebb5d6b7b80f7f3bec9899ad33db5
narasimhavuppala <nraovuppala@Wgmail.com>
2018-09-30 02:18:42 -0500

    method refs: defined the collections size as Supplier

--- Lambdas/src/java8/lambdas/MethodReferences.java
+++ Lambdas/src/java8/lambdas/MethodReferences.java
@@ -1,30 +1,34 @@
 package java8.lambdas;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
 
 public class MethodReferences {
 
 	public static void main(String[] args) {
-		
-		//Constructors
+
+		// Constructors
 
 		Supplier<Student> producer = Student::new;
 		System.out.println(producer.get());
 
-		//Static Methods
+		// Static Methods
 		Consumer<Integer> printit = System.out::println;
 		for (int i = 0; i < 2; i++) {
 			printit.accept(i);
 		}
 
 		Student st = new Student(1, "Narasimha", "Vuppala", "nraovuppala@gmail.com", null);
-		
-		//Instance methods
-		Supplier<Integer> c=st::getId;
+
+		// Instance methods
+		Supplier<Integer> c = st::getId;
 		System.out.println(c.get());
-		
-		
+
+		List<Integer> list = new ArrayList<>();
+
+		Supplier<Integer> supplierSize = list::size;
 
 	}
 
