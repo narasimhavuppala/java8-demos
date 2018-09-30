c3ec4550fd50c9c42cfdf40d63c3d9a1eb396f5b
narasimhavuppala <nraovuppala@Wgmail.com>
2018-09-29 23:14:45 -0500

    Functional Interfaces code is updated

--- Functional-01/src/java8/functional/CalcAdd.java
+++ Functional-01/src/java8/functional/CalcAdd.java
@@ -1,31 +1,62 @@
 package java8.functional;
 
 import java.util.function.BiFunction;
-import java.util.function.Function;
+import java.util.function.BinaryOperator;
+import java.util.function.Consumer;
 import java.util.function.DoubleFunction;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+import java.util.function.UnaryOperator;
 
 public class CalcAdd {
 
 	public static void main(String[] args) {
+
+		// SOLID : I stands for Interface Segregation
 		// Defining add methods
-		Function<Integer, Integer> add = (x) -> x + 1;
+		// Why Should we learn functional interfaces
+		// Any relation with Lambdas
+		Function<Integer, String> add = (x) -> x + "one";
 		System.out.println(add.apply(2));
+		add = (x) -> {
+			return x + "one";
+		};
 
-		Function<String, String> concat = (x) -> x + 1;
-		System.out.println(concat.apply("Test-"));
+		/****************** Sepcial Funtions ****************************/
+		// Supplier: Fetch config details
+		Supplier<Integer> numberGenerator = () -> 5;
+		System.out.println(numberGenerator.get());
 
-		// Calculator
-		Function<Integer, Function<Integer, Integer>> calcAdd = x -> y -> x + y;
+		// Consumer: Logging ,Sending to database
+		Consumer<String> printToConsole = x -> System.out.println(x);
+
+		printToConsole.accept("hello");
+
+		// Test the boolean condition
+		Predicate<Boolean> isValid = (x) -> !x;
+		System.out.println(isValid.test(false));
+		// isValid.and(isValid).and(isValid).and(isValid)
 
 		// Bi Function Usage
-		BiFunction<Integer, Integer, Integer> Biadd = (x, y) -> {
-			Integer u = x + y;
-			return u;
+		BiFunction<Integer, String, Double> Biadd = (x, y) -> {
+			System.out.println(String.format("x=%s and y=%s", x, y));
+			return 0.5;
 		};
-		System.out.println(Biadd.apply(1, 2));
+
+		Biadd.apply(4, "test");
+
+		// Unary Operator
+
+		UnaryOperator<String> upperCase = x -> x.toUpperCase();
+		System.out.println(upperCase.apply("test"));
+
+		BinaryOperator<Integer> biadd = (x, y) -> (x + y);
+		System.out.println(biadd.apply(2, 5));
 
 		// Double Function
-		DoubleFunction<String> df = (x) -> x + " Conerting double to String";
+		
+		DoubleFunction<String> df = (x) -> x + " Converting double to String";
 		System.out.println(df.apply(5));
 
 	}
