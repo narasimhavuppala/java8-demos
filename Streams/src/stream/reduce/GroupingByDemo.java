053aa17d38da5a6a388466f840c55007895770ed
Preetham <preethamreddyboddu12@gmail.com>
2022-04-18 23:02:56 +0530

    Added Grouping By Demo

---
+++ Streams/src/stream/reduce/GroupingByDemo.java
@@ -0,0 +1,29 @@
+package stream.reduce;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import stream.Person;
+
+public class GroupingByDemo {
+
+	public static void main(String[] args) {
+
+		List<Person> persons = new ArrayList<>();
+		persons.add(new Person("Max", 18));
+		persons.add(new Person("Peter", 23));
+		persons.add(new Person("Pamela", 23));
+		persons.add(new Person("David", 12));
+		
+		//Getting Count for Each Age
+
+		Map<Integer, Long> data = persons.stream()
+				.collect(Collectors.groupingBy(Person::getAge, Collectors.counting()));
+		
+		System.out.println(data);
+
+	}
+
+}
