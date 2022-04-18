e0ef715b842e4a7e85828d725d9423beba71ce38
Preetham <preethamreddyboddu12@gmail.com>
2022-04-18 23:04:10 +0530

    Grouping by Examples

--- Streams/src/stream/reduce/GroupingByDemo.java
+++ Streams/src/stream/reduce/GroupingByDemo.java
@@ -3,6 +3,7 @@ package stream.reduce;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 import java.util.stream.Collectors;
 
 import stream.Person;
@@ -16,12 +17,16 @@ public class GroupingByDemo {
 		persons.add(new Person("Peter", 23));
 		persons.add(new Person("Pamela", 23));
 		persons.add(new Person("David", 12));
-		
-		//Getting Count for Each Age
+
+		// Getting Count for Each Age
 
 		Map<Integer, Long> data = persons.stream()
 				.collect(Collectors.groupingBy(Person::getAge, Collectors.counting()));
-		
+		System.out.println(data);
+
+		//Be Defualt uses HashMap. We can override with the TreeMap or LinkedHashMap etc
+		data = persons.stream().collect(Collectors.groupingBy(Person::getAge,TreeMap::new, Collectors.counting()));
+
 		System.out.println(data);
 
 	}
